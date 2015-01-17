0b9b2dadce8f5149546b00b7426c5c82cdcadac7
Kaldoran <kaldoran@live.fr>
2015-01-17 19:09:40 +0100

    Essai Spring security Fail n°1

---
+++ PWA_Bank_Part1/src/main/java/com/partOne/config/AppConfig.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.partOne.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author kaldoran
+ */
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.partOne.web.*" })
+@Import({ SecurityConfig.class })
+public class AppConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver
+                = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+ 
+}
---
+++ PWA_Bank_Part1/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,14 @@
+<%-- 
+    Document   : hello
+    Created on : 17 janv. 2015, 18:28:08
+    Author     : kaldoran
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Title : ${title}</h1>	
+	<h1>Message : ${message}</h1>	
+</body>
+</html>
